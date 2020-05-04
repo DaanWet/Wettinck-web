88554982427df78a6ecb5a274a3106c8817b54ef
dwettinc <daan.wettinck@ugent.be>
2020-05-04 02:54:58 +0200

    Added Social Page

--- personal-site/src/router/index.js
+++ personal-site/src/router/index.js
@@ -18,6 +18,11 @@ Vue.use(VueRouter)
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Projects.vue')
   },
+  {
+    path: '/social',
+    name: "Social",
+    component: () => import('../views/Social.vue')
+  }
 ]
 
 const router = new VueRouter({
---
+++ personal-site/src/views/Social.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="socials">
+  <a href="https://github.com/DaanWet">
+    <figure>
+      <img src="../assets/GitHub-Mark-120px-plus.png" alt="Github logo"/>
+      <figcaption>
+        My GitHub Profile
+      </figcaption>
+    </figure>
+  </a>
+  <a href="https://steamcommunity.com/profiles/76561198185927173/">
+    <figure>
+      <img src="../assets/steam-logo-transparent.png" alt="Steam logo"/>
+      <figcaption>
+        My Steam Profile
+      </figcaption>
+    </figure>
+  </a>
+  <a href="https://discord.gg/9EncUC5">
+    <figure>
+      <img src="../assets/Discord-Logo-Color.svg" alt="Discord logo"/>
+      <figcaption>
+        Come chat with me on Discord
+      </figcaption>
+    </figure>
+  </a>
+  <a href="https://www.facebook.com/Dazed00">
+    <figure>
+      <img src="../assets/facebook-logo.png" alt="Discord logo"/>
+      <figcaption>
+        My Facebook Page
+      </figcaption>
+    </figure>
+  </a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.socials {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin: 50px 150px;
+}
+.socials a {
+  text-align: center;
+  margin: auto;
+  text-decoration: none;
+  color:black
+}
+
+figure img{
+  width: 100px;
+}
+
+
+figcaption {
+  margin-top: 10px
+}
+</style>
