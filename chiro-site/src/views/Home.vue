bae5176d571883bb52053e11ebe75fd720fa9087
dwettinc <daan.wettinck@ugent.be>
2020-05-26 12:00:27 +0200

    Added chiro site template

---
+++ chiro-site/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img src="../assets/bus.jpg"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+
+<style>
+img {
+  width: 100%
+}
+
+</style>
+
